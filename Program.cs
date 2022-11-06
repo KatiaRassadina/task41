174ede04da05c08d76cbd6e684d063299e8c73bf
KatiaRassadina <rassadinaekaterina877@gmail.com>
2022-11-06 14:00:12 +0300

    task done

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Пользователь воодит М чисел, посчитайте сколько чисел больше 0 ввел пользователь
+Console.WriteLine("input digits, devided by comma");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(","),int.Parse);
+int index = 0;
+for(int i=0; i<arr.Length; i++)
+{
+    if(arr[i]>0)
+    {index++;}
+}
+Console.WriteLine("The number of digits > 0 is "+(index));
